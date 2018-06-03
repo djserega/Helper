35e879a252e8242fcaa01cd1f97fa663688fb378
dj_serega <galyuk.sergey@gmail.com>
2018-06-03 23:48:31 +0300

    dev

---
+++ src/Helper/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    internal delegate void GlobalHotKeyEvent();
+
+    internal class GlobalHotKeyEvents : EventArgs
+    {
+        internal event GlobalHotKeyEvent GlobalHotKeyOpenFormMessage;
+
+        internal void EvokeOpenFormMessageEvent()
+        {
+            if (GlobalHotKeyOpenFormMessage == null)
+                return;
+
+            GlobalHotKeyOpenFormMessage();
+        }
+
+    }
+}
